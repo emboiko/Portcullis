45e66ab40a215b6dc87c725ed6f247f13c140f2f
Edward Boiko <Emboiko@gmail.com>
2020-01-25 23:36:23 -0500

    remove pyinstaller.bat

--- src/pyinstaller.bat
+++
@@ -1,7 +0,0 @@
-PyInstaller ^
-C:\Programming\C++\Arduino\Projects\Portcullis\src\portculler.py ^
--F ^
--n Portculler ^
--p C:\Programming\Python\Environments\PySerial\Lib\site-packages\ ^
--i C:\Users\emboi\Downloads\26631684.ico ^
---distpath ./dist_winx64
